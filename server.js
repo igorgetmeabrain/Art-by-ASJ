0858d2406f26e50e027330b5e9ab0760566cd0fe
Doug Badger <dougbadger@hotmail.com>
2022-12-30 20:45:57 +0000

    Setup node and server

---
+++ server.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/api.js');
+
+const app = express();
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Index page (static HTML)
+app.route('/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+  }); 
+
+// User routes
+userRoutes(app);
+    
+// 404 Not Found Middleware
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
+
+const portNum = process.env.PORT || 3000;
+
+// Start our server and tests!
+app.listen(portNum, () => {
+  console.log(`Listening on port ${portNum}`);
+});
